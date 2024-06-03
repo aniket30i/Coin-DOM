0eb734985755c79d6429f7ae737dcc438870ed15
aniket30i <iamaniket.saha.09@protonmail.com>
2024-06-03 20:14:31 +0530

    landing page positioning finished

--- coin-dom/src/components/LandingPage/MainComp/index.jsx
+++ coin-dom/src/components/LandingPage/MainComp/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./styles.css";
+import Button from "../../Button";
+import iphone from "../../../assets/iphone.png";
+import gradient from "../../../assets/gradient.png";
 
 function MainComp() {
   return (
@@ -7,8 +10,19 @@ function MainComp() {
       <div className="left-component">
         <h1 className="track-know-heading">Know & Track Crypto</h1>
         <h1 className="real-time-heading">Real Time.</h1>
+        <p className="info-text">
+          Learn and Track crypto through public apis in real time. Start your
+          journey with a click on Educate.
+        </p>
+        <div className="btn-flex">
+          <Button text={"Dashboard"} />
+          <Button outlined={true} text={"Share"} />
+        </div>
       </div>
-      <div className="">phone</div>
+      <div className="phone-container">
+        <img src={iphone} className="iphone" />
+        <img src={gradient} className="gradient" />
+      </div>
     </div>
   );
 }
