f9649c2a7b1ff9dc070e61a2781897b57367b47b
aniket30i <iamaniket.saha.09@protonmail.com>
2024-06-06 18:03:18 +0530

    graph plotting

--- coin-dom/src/pages/CoinPage.jsx
+++ coin-dom/src/pages/CoinPage.jsx
@@ -8,12 +8,15 @@ import List from "../components/Dashboard/List";
 import CoinInfo from "../components/Coin";
 import { getCoinData } from "../functions/getCoinData";
 import { getCoinPrices } from "../functions/getCoinPrices";
+import LineChart from "../components/Coin/LineChart";
+import { convertDate } from "../functions/convertDate";
 
 function CoinPage() {
   const { id } = useParams();
   const [loading, isLoading] = useState(true);
   const [coinData, setCoinData] = useState([]);
-  const [days, setDays] = useState(30);
+  const [days, setDays] = useState(120);
+  const [chartData, setchartData] = useState({});
   useEffect(() => {
     if (id) {
       getData();
@@ -27,6 +30,23 @@ function CoinPage() {
       const prices = await getCoinPrices(id, days);
       if (prices.length > 0) {
         console.log("yes");
+
+        setchartData({
+          labels: prices.map((price) => convertDate(price[0])),
+          datasets: [
+            {
+              data: prices.map((dates) => dates[1]),
+              borderColor: "#9400ff",
+              backgroundColor: "transparent",
+              borderWidth: 2,
+              fill: true,
+              tension: 0.25,
+              backgroundColor: "rgba(148, 0, 255, 0.2)",
+              borderColor: "#9400ff",
+              pointRadius: 0,
+            },
+          ],
+        });
         isLoading(false);
       }
     }
@@ -40,6 +60,9 @@ function CoinPage() {
         <>
           <div className="grey-wrapper">
             <List coin={coinData} />
+          </div>
+          <div className="grey-wrapper">
+            <LineChart chartData={chartData} />
           </div>
           <CoinInfo heading={coinData.name} desc={coinData.desc} />
         </>
